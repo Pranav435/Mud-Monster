3f42abc43365b5be8a82079c0973c8b49ac2528e
Pranav <pranavsavla2003@gmail.com>
2019-11-26 21:06:36 +0530

    Worked on printing error and winning message

---
+++ Main.py
@@ -0,0 +1,5 @@
+#CS50 project
+
+#General game rules:
+#Once the game starts, the only category available for now will be movies, available from the IMDB database. First, the word will be presented to the user without any hints. The user can ask for three hints, with the consequence of their score being decremented by a certain amount
+#Three hints are allowed, where in each hint, a letter is added randomly to the word.
--- project_V4.py
+++ project_V4.py
@@ -20,16 +20,16 @@ letters=[]
 # if the user guesses a letter that they have already guessed or guesses something that is
 # not a valid character, they are prompted to guess again
 def getguess():
-    while True:
-        guess=input("Are you ready? Enter a single letter")
-        guess=guess.lower()
+#    while True:
+    guess=input("Are you ready? Enter a single letter")
+    guess=guess.lower()
 #        lettersguessed=guess
-        if guess in letters:
-            print("Already guessed")
-            continue
+    if guess in letters:
+        print("Already guessed")
+#           continue
 #        letters=[]
         letters.append(guess)
-
+    return guess
 # getprintversion will get the print version of the secret word.  
 # If a letter in the word is already guessed, that letter will be printed
 # If a letter in the word is not guessed, a * will be printed in that place
@@ -42,24 +42,51 @@ def getprintversion(secretword):
             display=display+"*"
     return display
     # printrules will print the rules of the game
-def printrules():
+"""def printrules():
     print("Helow! Welcome to the word guessing game! I am the mud monster!")
+    time.sleep(0.5)
     print("I've chosen a random word. You'll have to enter a single character")
+    time.sleep(0.5)
     print("I will tell you  the number of characters that word contains by replacing each character with a *. ")
+    time.sleep(0.5)
     print("If your guess is correct, I'll replace * with that letter and print it for you. ")
+    time.sleep(0.5)
     print("You will get 6 chances to guess my secret word. ")
-    print("If you guess a wrong character, I will cover a part of your body with mud! If you lose all chances, you'll be fully covered with mud!")
+    time.sleep(0.5)
+    print("If you guess a wrong character, I will cover a part of your body with mud! If you lose all chances, you'll be fully covered with mud!")"""
 
 # print error message
 def printmud():
     pass
     
 # start of the main program
-printrules()
+#printrules()
 
 #initializations
-wordlist1 = ['independence','freedom','fighters','holiday','flag','india','august','british','nation','july','celebration']
+#wordlist1 = ['independence','freedom','fighters','holiday','flag','india','august','british','nation','july','celebration']
+file=open("movies.txt")
+wordlist=file.read().split("\n")
+print(wordlist)
 
-getguess()
+#getguess()
+word=choosesecretword(wordlist)
 #start your code here
-input()
+#input()
+
+def hint():
+    pass
+userword=""
+def error_message():
+    playerChoice=getguess()
+    while win==False:
+        if playerChoice in word:
+#            pass
+            userword+=playerChoice
+        else:
+            print("Sorry, the letter you chose is not in the chosen word.")
+        if userword==word:
+
+
+error_message()
+#Also need to change getprintversion so that it prints the word partially
+#HW for PRanav: Read up on sets, any function
